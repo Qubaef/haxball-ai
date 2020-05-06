e48322c4367f3ed21f176d0548650fdd862876a7
Etav99 <etavpl@gmail.com>
2020-05-06 21:52:11 +0200

    Removed old code

--- AiEnviroment.py
+++ AiEnviroment.py
@@ -4,18 +4,6 @@ from AgentDQN import AgentDQN
 env = GameController()
 agent = AgentDQN(len(env.get_state()), 32)
 
-input_player1 = [None] * 4
-input_player1[0] = 0
-input_player1[1] = 0
-input_player1[2] = 0
-input_player1[3] = 0
-
-input_player2 = [None] * 4
-input_player2[0] = 7
-input_player2[1] = 0
-input_player2[2] = 0
-input_player2[3] = 0
-
 state = env.get_state()
 while(True):
     input_player1 = agent.make_move(state)
